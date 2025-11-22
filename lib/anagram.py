f6fec9cfb75770ef2f3d0165266297070a1e2cbc
sasha lisa <lisasashaa@gmail.com>
2025-11-22 13:22:10 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, words):
+        # Sort the letters of the initialized word
+        sorted_word = sorted(self.word)
+        
+        # Filter words that have the same sorted letters
+        matches = []
+        for word in words:
+            if sorted(word.lower()) == sorted_word and word.lower() != self.word:
+                matches.append(word)
+        
+        return matches
